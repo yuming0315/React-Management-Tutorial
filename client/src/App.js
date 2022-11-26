b21bb673605c1d975425b99c5415970b9d1835ab
yuming0315 <gjdbal111@naver.com>
2022-11-26 12:35:09 +0900

    getpost

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
+import Customer from "./components/Customer"
 import Head from "./components/Head";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
-
+import TableCell from "@mui/material/TableCell";
 
 function App() {
+  const [state , setState] = useState({customers:""});
+
+  function componentDidMount(){
+    callApi().then(res => setState({customers: res}))
+    .catch(err=>console.log(err));
+    return state&&true;
+  }
+
+  const callApi = async() =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   return (
     <Paper className="paper">
       <Table className="table">
         <Head />
-        
+        {componentDidMount()?state.customers.map(c=>{return(<Customer key={c.id}         props={c} /> )}):""}
+        {state.customers.map(c=><Customer key={c.id} props={c} />)}
       </Table>
     </Paper>
   );
--- server.js
+++ server.js
@@ -6,8 +6,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello', (req,res)=>{
-    res.send({message: 'hello Express!'});
+app.get('/api/customers',(req,res)=>{
+    res.send([
+        {
+        'id':1,
+        'image':'https://placeimg.com/64/64/1',
+        'name':'나동빈',
+        'birthday':'961222',
+        'gender':'남자',
+        'job':'대학생'
+        },
+        {
+            'id':2,
+            'image':'https://placeimg.com/64/64/1',
+            'name':'나동빈',
+            'birthday':'961222',
+            'gender':'남자',
+            'job':'대학생'
+        }
+    ])
 })
 
 app.listen(port,()=>console.log(`listen on port ${port}`));
